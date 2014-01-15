e7c558bf5c9a9e1d9254305d53e6d94566bd9afa
Jeff Cross <middlefloor@gmail.com>
2014-01-15 13:36:46 -0800

    style: add some basic styling to form

--- nutritional-info-component.js
+++ nutritional-info-component.js
@@ -2,7 +2,7 @@ angular.module('jcNutritionalInfo', []).
   directive('jcNutritionalInfo', ['$parse', function ($parse) {
     return {
       restrict: 'E',
-      template: '<div><div ng-if="!nutritionData || !nutritionData.length">Loading nutritional information</div><table><tbody><tr ng-repeat="group in nutritionData"><td><div class="nutrition-heading"><strong ng-bind="group.name"></strong><strong class="pull-right" ng-bind="group.total"></strong><table ng-if="group.children"><tbody><tr ng-repeat="child in group.children"><td><strong ng-bind="child.name"></strong><strong class="pull-right" ng-bind="child.total"></strong></td></tr></tbody></table></td></tr></tbody></table></div>',
+      template: '<div><div ng-if="!nutritionData || !nutritionData.length">Loading nutritional information</div><table width="100%" cellspacing="0" cellpadding="0"><tbody><tr ng-repeat="group in nutritionData"><td><div class="nutrition-heading"style="display:block; background:#000;color:#fff;font-size:18px; line-height:24px;height:30px; padding:3px 6px;"><strong ng-bind="group.name"></strong><strong class="pull-right" ng-bind="group.total"></strong></div><table class="table" ng-if="group.children"><tbody><tr ng-repeat="child in group.children"><td><strong ng-bind="child.name"></strong><strong class="pull-right" ng-bind="child.total"></strong></td></tr></tbody></table></td></tr></tbody></table></div>',
       scope: {
         nutritionData: "="
       }
